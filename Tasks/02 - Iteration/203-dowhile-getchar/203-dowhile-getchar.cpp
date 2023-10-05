ad31b5576da1326ae13011ccbe3b24016f2346c7
FeintWitch <feintwitch@gmail.com>
2023-10-05 15:48:50 +0100

    Lab2-
    done 50% of the work

--- Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
+++ Tasks/02 - Iteration/203-dowhile-getchar/203-dowhile-getchar.cpp
@@ -13,9 +13,13 @@ int main()
         userEnteredCharacter = getchar();   //ASCII character
         returnKey = getchar();              //Return key
         printf("You typed %c\n", userEnteredCharacter);
-        //printf("You also pressed return (ASCII %d)\n", returnKey);    //Uncomment this
-
-    } while (userEnteredCharacter != 'q');            //Repeat if condition is met - note the != operator
-
+            //Task 2 - Pressed and sent the letter 'a' in the terminal, responded by saying "you typed a"
+        printf("You also pressed return (ASCII %d)\n", returnKey);    
+            //The extra line responded with You also pressed return (ASCII 10) 
+            //What is the ascii code for the return code? : The accii code for return is const[36]
+           
+    } while ((userEnteredCharacter != 'q') && (userEnteredCharacter != 'Q'));       //Repeat if condition is met - note the != operator
+        // I added '&&' which is a AND. I reused the veriable 'UserEnteredCharacter' and operator to Q
+                //Could've done  'existCondition = (userEntered Character == 'q') || (userEnteredCharacter == 'Q');
     puts("Finished");
 }
--- Tasks/02 - Iteration/205-while-loop/205-while-loop.cpp
+++ Tasks/02 - Iteration/205-while-loop/205-while-loop.cpp
@@ -6,7 +6,8 @@ int main()
 
     puts("Start while loop");
 
-    while (i < 5)
+    while (i < 6)
+        //Orinigally 5 but switched to 6 
     {
         printf("i = %d\n", i);
         i++;        // same as i = i + 1 but more concise;
@@ -15,3 +16,23 @@ int main()
     printf("Exiting with i=%d\n", i);
 }
 
+//Task 2. Setting the breakpoint shows the code is the looping around from 0 to 5, when it exists 
+//Task 3. How many times does the loop repeat? : It repeats 5 times 
+//Task 4. Final  value of i when the code exists : 5, While (i < 5) sets up that anything before 5 is printed as soon it hits 5 it exits since the line 'printf("Exiting with i=%d\n" , i' 
+//Task 5. Change the initial value of the variable i to 6 and step through
+    //what do you notice that is different> : Instead of existing the console wen it hits 5, now it does 6. So it counts 1-5 this time
+//Task 6. Modify the code to count down from 10 to 0 of 2 
+
+int main()
+    {
+        int i = -10;
+
+        while (i >= 0)
+        {
+            printf("i =%d\n", it);
+            i -= 2; 
+
+        }
+
+        printf("exiting with i=&d\n", i);
+    })
